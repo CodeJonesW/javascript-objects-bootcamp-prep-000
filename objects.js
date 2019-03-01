dbcbdccdcbe2e8a822bc53417c84c23e3fbc4b3c
Will Jones <willjones2010@gmail.com>
2019-03-01 01:52:56 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
 var playlist = {}
 
 function updatePlaylist (obj,key,song) {
-  obj.key = 
+  obj.key = 'song';
   return obj;
 }
 function removeFromPlaylist (obj,key) {
-  delete obj.str;
+  delete obj.key;
 }
